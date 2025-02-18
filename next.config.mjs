cb683e523d745a27b653d4f500acedd131e6a862
round2fight <rosdancodes@gmail.com>
2025-02-18 16:15:56 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/paintwater.mp4", // Path to your video file in public/
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // Cache for 1 year
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
